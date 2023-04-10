8a104db9271661f1b2581df77b94807c5d13c12f
cerosyunos10 <cerosyunos10@gmail.com>
2023-04-10 14:58:28 +0200

    Documentar la información general de la API

--- calculadora/src/main/java/com/demo/calculadora/infraestructure/config/CalculadoraConfiguration.java
+++ calculadora/src/main/java/com/demo/calculadora/infraestructure/config/CalculadoraConfiguration.java
@@ -3,10 +3,24 @@ package com.demo.calculadora.infraestructure.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.corp.calculator.TracerImpl;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 
 @Configuration
 public class CalculadoraConfiguration {
+	
+	@Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Calculadora API")
+                        .version("1.0.0")
+                        .description("Ejemplo de calculadora Spring Boot 3 con Swagger")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
 
 	@Bean
 	public TracerImpl tracer() {
